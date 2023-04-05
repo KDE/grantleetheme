22ec95fb6a43d1b7f847dda8e88d15ed169e230a
Laurent Montel <montel@kde.org>
2023-04-05 08:39:41 +0200

    Remove code qt < 6.0.0

--- src/plugin/icon.cpp
+++ src/plugin/icon.cpp
@@ -6,33 +6,19 @@
  */
 
 #include "icon.h"
-#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-#include <grantlee/exception.h>
-#include <grantlee/parser.h>
-#include <grantlee/variable.h>
-#else
 #include <KTextTemplate/Exception>
 #include <KTextTemplate/Parser>
 #include <KTextTemplate/Variable>
-#endif
 
 #include <KIconLoader>
 
 IconTag::IconTag(QObject *parent)
-#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-    : Grantlee::AbstractNodeFactory(parent)
-#else
     : KTextTemplate::AbstractNodeFactory(parent)
-#endif
 {
 }
 
 IconTag::~IconTag() = default;
-#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-Grantlee::Node *IconTag::getNode(const QString &tagContent, Grantlee::Parser *p) const
-#else
 KTextTemplate::Node *IconTag::getNode(const QString &tagContent, KTextTemplate::Parser *p) const
-#endif
 {
     Q_UNUSED(p)
 
@@ -50,18 +36,10 @@ KTextTemplate::Node *IconTag::getNode(const QString &tagContent, KTextTemplate::
     const QStringList parts = smartSplit(tagContent);
     const int partsSize = parts.size();
     if (partsSize < 2) {
-#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-        throw Grantlee::Exception(Grantlee::TagSyntaxError, QStringLiteral("icon tag takes at least 1 argument"));
-#else
         throw KTextTemplate::Exception(KTextTemplate::TagSyntaxError, QStringLiteral("icon tag takes at least 1 argument"));
-#endif
     }
     if (partsSize > 4) {
-#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-        throw Grantlee::Exception(Grantlee::TagSyntaxError, QStringLiteral("icon tag takes at maximum 3 arguments, %1 given").arg(partsSize));
-#else
         throw KTextTemplate::Exception(KTextTemplate::TagSyntaxError, QStringLiteral("icon tag takes at maximum 3 arguments, %1 given").arg(partsSize));
-#endif
     }
 
     int sizeOrGroup = KIconLoader::Small;
@@ -90,20 +68,12 @@ KTextTemplate::Node *IconTag::getNode(const QString &tagContent, KTextTemplate::
 }
 
 IconNode::IconNode(QObject *parent)
-#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-    : Grantlee::Node(parent)
-#else
     : KTextTemplate::Node(parent)
-#endif
 {
 }
 
 IconNode::IconNode(const QString &iconName, int sizeOrGroup, const QString &altText, QObject *parent)
-#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-    : Grantlee::Node(parent)
-#else
     : KTextTemplate::Node(parent)
-#endif
     , mIconName(iconName)
     , mAltText(altText)
     , mSizeOrGroup(sizeOrGroup)
@@ -111,11 +81,7 @@ IconNode::IconNode(const QString &iconName, int sizeOrGroup, const QString &altT
 }
 
 IconNode::~IconNode() = default;
-#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-void IconNode::render(Grantlee::OutputStream *stream, Grantlee::Context *c) const
-#else
 void IconNode::render(KTextTemplate::OutputStream *stream, KTextTemplate::Context *c) const
-#endif
 {
     Q_UNUSED(c)
 
@@ -123,23 +89,11 @@ void IconNode::render(KTextTemplate::OutputStream *stream, KTextTemplate::Contex
     if (iconName.startsWith(QLatin1Char('"')) && iconName.endsWith(QLatin1Char('"'))) {
         iconName = iconName.mid(1, iconName.size() - 2);
     } else {
-#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-        const QVariant val = Grantlee::Variable(mIconName).resolve(c);
-#else
         const QVariant val = KTextTemplate::Variable(mIconName).resolve(c);
-#endif
-#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-        if (val.type() == QVariant::String) {
-#else
         if (val.metaType().id() == QMetaType::QString) {
-#endif
             iconName = val.toString();
         } else {
-#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-            iconName = val.value<Grantlee::SafeString>().get();
-#else
             iconName = val.value<KTextTemplate::SafeString>().get();
-#endif
         }
     }
 
@@ -148,25 +102,13 @@ void IconNode::render(KTextTemplate::OutputStream *stream, KTextTemplate::Contex
         if (mAltText.startsWith(QLatin1Char('"')) && mAltText.endsWith(QLatin1Char('"'))) {
             altText = mAltText.mid(1, mAltText.size() - 2);
         } else {
-#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-            const QVariant v = Grantlee::Variable(mAltText).resolve(c);
-#else
             const QVariant v = KTextTemplate::Variable(mAltText).resolve(c);
-#endif
             if (v.isValid()) {
-#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-                if (v.canConvert<Grantlee::SafeString>()) {
-                    altText = v.value<Grantlee::SafeString>().get();
-                } else {
-                    altText = v.toString();
-                }
-#else
                 if (v.canConvert<KTextTemplate::SafeString>()) {
                     altText = v.value<KTextTemplate::SafeString>().get();
                 } else {
                     altText = v.toString();
                 }
-#endif
             }
         }
     }
@@ -176,9 +118,5 @@ void IconNode::render(KTextTemplate::OutputStream *stream, KTextTemplate::Contex
             .arg(KIconLoader::global()->iconPath(iconName, mSizeOrGroup < KIconLoader::LastGroup ? mSizeOrGroup : -mSizeOrGroup))
             .arg(mSizeOrGroup < KIconLoader::LastGroup ? KIconLoader::global()->currentSize(static_cast<KIconLoader::Group>(mSizeOrGroup)) : mSizeOrGroup)
             .arg(altText.isEmpty() ? iconName : altText, altText); // title is intentionally blank if no alt is provided
-#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-    (*stream) << Grantlee::SafeString(html, Grantlee::SafeString::IsSafe);
-#else
     (*stream) << KTextTemplate::SafeString(html, KTextTemplate::SafeString::IsSafe);
-#endif
 }
