65d16fc6eb735615c95d87abfebeb3c768ab6999
Laurent Montel <montel@kde.org>
2023-12-26 07:45:07 +0100

    Fix the translation template version number (add 6 for kf6)
    Make sure that it will not conflict with qt5 version.
    
    @teams/localization

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
-$XGETTEXT `find . -name '*.cpp' -o -name '*.h'` -o $podir/libgrantleetheme.pot
+$XGETTEXT `find . -name '*.cpp' -o -name '*.h'` -o $podir/libgrantleetheme6.pot
 rm -f rc.cpp
