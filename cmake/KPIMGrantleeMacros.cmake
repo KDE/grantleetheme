394a14ecec8fc5bb6cca838d6746638dc724a435
Allen Winter <winter@kde.org>
2026-03-16 08:19:36 -0400

    KPIMGrantleeMacros.cmake - fix uninitialized variable warnings
    Grantlee5_VERSION_MAJOR and Grantlee5_VERSION_MINOR aren't set
    so they are unnecessary. Probably leftover from supporting both
    Qt5 and Qt6.

--- cmake/KPIMGrantleeMacros.cmake
+++ cmake/KPIMGrantleeMacros.cmake
@@ -5,21 +5,24 @@ macro(kpim_grantlee_adjust_plugin_name pluginname)
             PREFIX
                 ""
             LIBRARY_OUTPUT_DIRECTORY
-                "${CMAKE_BINARY_DIR}/bin/grantlee/${Grantlee5_VERSION_MAJOR}.${Grantlee5_VERSION_MINOR}"
+                "${CMAKE_BINARY_DIR}/bin/grantlee"
             RUNTIME_OUTPUT_DIRECTORY
-                "${CMAKE_BINARY_DIR}/bin/grantlee/${Grantlee5_VERSION_MAJOR}.${Grantlee5_VERSION_MINOR}"
+                "${CMAKE_BINARY_DIR}/bin/grantlee"
             DEBUG_POSTFIX
                 "d"
     )
+    if(NOT DEFINED CMAKE_CONFIGURATION_TYPES)
+        set(CMAKE_CONFIGURATION_TYPES)
+    endif()
     foreach(cfg ${CMAKE_CONFIGURATION_TYPES})
         string(TOUPPER ${cfg} CFG)
         set_target_properties(
             ${pluginname}
             PROPERTIES
                 LIBRARY_OUTPUT_DIRECTORY_${CFG}
-                    "${CMAKE_BINARY_DIR}/bin/grantlee/${Grantlee5_VERSION_MAJOR}.${Grantlee5_VERSION_MINOR}"
+                    "${CMAKE_BINARY_DIR}/bin/grantlee"
                 RUNTIME_OUTPUT_DIRECTORY_${CFG}
-                    "${CMAKE_BINARY_DIR}/bin/grantlee/${Grantlee5_VERSION_MAJOR}.${Grantlee5_VERSION_MINOR}"
+                    "${CMAKE_BINARY_DIR}/bin/grantlee"
         )
     endforeach()
 endmacro()
